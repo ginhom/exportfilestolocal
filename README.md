fcf08d8e0f541824d8a6ae0cc8ef5d1baecd6429
ginhom <jinhong.sky@gmail.com>
2019-12-30 16:29:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+# export files to ... README
+
+Right click on sidebar source control files,select a floder,then all selected files save to the selected floder.
+
+## Features
+
+sorry
+
+## Requirements
+
+fs
+
+## Extension Settings
+
+empty
+
+## Known Issues
+
+empty
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of ...
+
+
+
+## Working with Markdown
+
+**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+
+* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+
+### For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,63 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "helloword" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.exportToLocal', function (...resourceStates) {
+		// The code you place here will be executed every time your command is executed
+
+		const options = {
+			canSelectMany: false,
+			canSelectFiles: false,
+			canSelectFolders: true,
+			openLabel: '选择导出位置',
+			filters: {
+				'All files': ['*']
+			}
+		};
+		vscode.window.showOpenDialog(options).then(fileUri => {
+			if (fileUri && fileUri[0]) {
+				console.log('Selected file: ' + fileUri[0].fsPath);
+				// Display a message box to the user
+				const exportDir = fileUri[0].fsPath;
+				for (var rs of resourceStates) {
+					console.log(rs.resourceUri.path);
+					const fileName = path.basename(rs.resourceUri.path);
+					const resultPath = path.join(exportDir, fileName);
+					fs.writeFileSync(resultPath, fs.readFileSync(rs.resourceUri.path));
+					// vscode.window.showInformationMessage(`当前文件(夹)路径是：${rs.resourceUri.path}`);
+				}
+			}
+		});
+
+	});
+
+	context.subscriptions.push(disposable);
+	// context.subscriptions.push(vscode.commands.registerCommand('extension.getCurrentFilePath', (uri) => {
+	// 	vscode.window.showInformationMessage(`当前文件(夹)路径是：${uri ? uri.path : '空'}`);
+	// }));
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
